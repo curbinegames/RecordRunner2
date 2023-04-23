b07c2014f4905f581a5a51cb5abe10b8481cc7e9
curbine <joint.the.air@gmail.com>
2023-04-23 18:11:21 +0900

    Revert "make ddif cal proto"
    This reverts commit 375f68113410a1692f52217bc50764ad84e32383.

--- recp_cal_ddif.h
+++ recp_cal_ddif.h
@@ -4,11 +4,5 @@
 
 extern int cal_difkey(int n_time, int b_time, int n_note, int b_note, int
 	bb_note, int b_dif);
-extern void CalGhostSkip(note_box note1[], note_box note2[], note_box note3[],
-	short objNo[]);
-extern char CalFindNearNote(note_box note1[], note_box note2[],
-	note_box note3[]);
-extern int cal_ddif(int num, int const* difkey, int Etime, int noteoff,
-	int difsec, int voidtime);
 
 #endif
