0f4408036202cfa61b74076f8c071dca4cf42fb0
curbine <joint.the.air@gmail.com>
2023-05-11 19:25:45 +0900

    delete define.h

--- system.h
+++ system.h
@@ -5,9 +5,21 @@
 
 #define COLOR_WHITE (0xffffffff)
 
+#define OLD_RATE_NUM 10
+#define OLD_RATE_FILE_NAME_S L"save/rateS.dat" /* 名前 */
+#define OLD_RATE_FILE_NAME_N L"save/rateN.dat" /* 数値 */
+
+#define RATE_NUM 20
+#define RATE_FILE_NAME L"save/Prate.dat" /* 名前+数値 */
+
 #define INIT_PIC() InitGraph(); CutinReadyPic()
 #define INIT_SND() InitSoundMem(); CutinReadySnd()
 #define INIT_MAT() INIT_PIC(); INIT_SND()
+
+typedef struct play_rate_s {
+	wchar_t name[255] = L"\0";
+	double num = 0;
+} play_rate_t;
 
 typedef enum now_scene_e {
 	SCENE_TITLE = 0,
