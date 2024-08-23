5397bb8b821335fec1bc77574c7dccb181824d73
curbinegames <joint.the.air@gmail.com>
2024-08-23 10:37:25 +0900

    made class

--- helpBar.cpp
+++ helpBar.cpp
@@ -1,10 +1,12 @@
 
 #include "DxLib.h"
+#include "general/dxcur.h"
 #include "helpBar.h"
 #include "system.h"
+#include "option.h"
 #include "RecWindowRescale.h"
 
-static int HelpBar = 0;
+static DxPic_t HelpBar = 0;
 
 void InitHelpBar() {
 	HelpBar = LoadGraph(L"picture/help.png");
@@ -48,6 +50,65 @@ void DrawHelpBar(help_bar_mat_t mat, char lan) {
 		break;
 	case HELP_MAT_OPTION:
 		switch (lan) {
+		case 0: /* Japanese */
+			RecRescaleDrawString(5, 460, L"上下左右キー:選択   BackSpaceキー:保存して戻る", COLOR_WHITE);
+			break;
+		case 1: /* English */
+			RecRescaleDrawString(5, 460, L"←↓↑→ key:choose   BackSpace key:save the option and return", COLOR_WHITE);
+			break;
+		}
+		break;
+	}
+	return;
+}
+
+rec_helpbar_c::rec_helpbar_c() {
+	this->pic = LoadGraph(L"picture/help.png");
+}
+
+rec_helpbar_c::~rec_helpbar_c() {
+	DeleteGraph(this->pic);
+}
+
+void rec_helpbar_c::DrawHelp(help_bar_mat_t mat) {
+	int page = 0;
+	RecRescaleDrawGraph(0, 0, this->pic, TRUE);
+
+	switch (mat) {
+	case HELP_MAT_MUSIC_SELECT:
+		page = (int)(GetNowCount() / 10000);
+		switch (optiondata.lang) {
+		case 0: /* Japanese */
+			switch (page % 3) {
+			case 0:
+				RecRescaleDrawString(5, 460, L"Enterキー: 決定, BackSpaceキー: 戻る", COLOR_WHITE);
+				break;
+			case 1:
+				RecRescaleDrawString(5, 460, L"上下キー: 曲選択, 左右キー: 難易度選択", COLOR_WHITE);
+				break;
+			case 2:
+				RecRescaleDrawString(5, 460, L"Zキー: 楽曲を並び替える, Pキー+Enterキー: オートプレイ", COLOR_WHITE);
+				break;
+			}
+			break;
+		case 1: /* English */
+			switch (page % 3) {
+			case 0:
+				RecRescaleDrawString(5, 460, L"Enter key: start, BackSpace key: back to menu", COLOR_WHITE);
+				break;
+			case 1:
+				RecRescaleDrawString(5, 460, L"↑↓key: music select, ←→key: dif select", COLOR_WHITE);
+				break;
+			case 2:
+				RecRescaleDrawString(5, 460, L"Z key: Sort Songs, P and Enter key: Auto Play", COLOR_WHITE);
+				break;
+			}
+			break;
+		}
+		break;
+
+	case HELP_MAT_OPTION:
+		switch (optiondata.lang) {
 		case 0: /* Japanese */
 			RecRescaleDrawString(5, 460, L"上下左右キー:選択   BackSpaceキー:保存して戻る", COLOR_WHITE);
 			break;
--- helpBar.h
+++ helpBar.h
@@ -1,10 +1,21 @@
 #pragma once
 
+#include "general/dxcur.h"
+
 typedef enum help_bar_mat_e {
 	HELP_MAT_MUSIC_NONE = 0,
 	HELP_MAT_MUSIC_SELECT,
 	HELP_MAT_OPTION,
 } help_bar_mat_t;
+
+class rec_helpbar_c {
+private:
+	DxPic_t pic;
+public:
+	rec_helpbar_c();
+	~rec_helpbar_c();
+	void DrawHelp(help_bar_mat_t mat);
+};
 
 extern void InitHelpBar();
 extern void DrawHelpBar(help_bar_mat_t mat, char lan);
