5edbfe44d73670428a8d99e4a0656d085af2f0b8
curbinegames <joint.the.air@gmail.com>
2024-09-23 23:14:06 +0900

    update system

--- system.h
+++ system.h
@@ -5,10 +5,10 @@
 #include "helpBar.h"
 
 #define WINDOW_SIZE_X 1280                     // ウィンドウの横のサイズ
-#define WINDOW_SIZE_Y (WINDOW_SIZE_X * 9 / 16) // ウィンドウの縦のサイズ
+#define WINDOW_SIZE_Y (WINDOW_SIZE_X * 9 / 16) // ウィンドウの縦のサイズ 720
 
 #define OLD_WINDOW_SIZE_X 640                         // 前バージョンのウィンドウの横のサイズ
-#define OLD_WINDOW_SIZE_Y (OLD_WINDOW_SIZE_X * 3 / 4) // 前バージョンのウィンドウの縦のサイズ
+#define OLD_WINDOW_SIZE_Y (OLD_WINDOW_SIZE_X * 3 / 4) // 前バージョンのウィンドウの縦のサイズ 480
 
 #define COLOR_WHITE (0xffffffff)
 
@@ -40,6 +40,7 @@ typedef enum now_scene_e {
 	SCENE_OPTION,
 	SCENE_EXIT = 5,
 	SCENE_MUSIC,
+	SCENE_RESULT,
 } now_scene_t; /* = next */
 
 extern int SmallFontData;
