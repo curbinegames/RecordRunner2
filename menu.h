e4f73f73b5759732e111fdffdc5e80f1819b7b51
curbinegames <joint.the.air@gmail.com>
2024-11-18 15:37:23 +0900

    made rec_manu_dataset_c

--- menu.h
+++ menu.h
@@ -2,7 +2,7 @@
 #include "recr_cutin.h"
 #include "RecWindowRescale.h"
 
-#define MENU_NUM sizeof(menu_item) / sizeof(menu_item[0])
+#define MENU_NUM (sizeof(menu_item) / sizeof(menu_item[0]))
 
 typedef struct rec_menu_data_s {
 	DxPic_t card;
@@ -38,8 +38,96 @@ static rec_menu_data_t menu_item[] = {
 	}
 };
 
-void DrawBack(int now, int before, int time);
-void DrawCard(int num, int LR, int time);
+static class rec_manu_dataset_c {
+private:
+	int LR = 1;
+	int Ncmd = 0;
+	int Bcmd = 0;
+	DxTime_t stime = -250;
+
+	void DrawBack(void) const {
+		int time = GetNowCount() - this->stime;
+		time = betweens(0, time, 250);
+		RecRescaleDrawGraph(0, 0, menu_item[this->Bcmd].back, TRUE);
+		SetDrawBlendMode(DX_BLENDMODE_ALPHA, lins(0, 0, 250, 255, time));
+		RecRescaleDrawGraph(0, 0, menu_item[this->Ncmd].back, TRUE);
+		SetDrawBlendMode(DX_BLENDMODE_ALPHA, 255);
+		RecRescaleDrawGraph(0, pals(250, 240, 0, 0, time), menu_item[this->Bcmd].ground, TRUE);
+		RecRescaleDrawGraph(0, pals(250, 0, 0, 240, time), menu_item[this->Ncmd].ground, TRUE);
+		return;
+	}
+
+	void DrawCard(void) const {
+		int time = GetNowCount() - this->stime;
+		int num = this->Ncmd;
+		time = betweens(0, time, 250);
+		num -= 2;
+		while (num < 0) {
+			num += MENU_NUM;
+		}
+		for (int i = 0; i < 5; i++) {
+			int drawX = pals(250, 420 * i - 680, 0, 420 * i + 420 * this->LR - 680, time);
+			int drawY = pals(250, 100, -250, 0, -time * this->LR + 250 * i + 250 * this->LR - 250);
+			RecRescaleDrawGraph(drawX, drawY, menu_item[num].card, TRUE);
+			num = (num + 1) % MENU_NUM;
+		}
+		return;
+	}
+
+public:
+	rec_manu_dataset_c(void) {
+		menu_item[0].card = LoadGraph(L"picture/menu/FREE PLAY.png");
+		menu_item[0].ground = LoadGraph(L"picture/menu/FREE PLAY G.png");
+		menu_item[0].back = LoadGraph(L"picture/backskynoamal.png");
+		menu_item[1].card = LoadGraph(L"picture/menu/COLLECTION.png");
+		menu_item[1].ground = LoadGraph(L"picture/menu/COLLECT G.png");
+		menu_item[1].back = LoadGraph(L"picture/menu/COLLECT W.png");
+		menu_item[2].card = LoadGraph(L"picture/menu/OPTION.png");
+		menu_item[2].ground = LoadGraph(L"picture/menu/OPTION G.png");
+		menu_item[2].back = LoadGraph(L"picture/menu/OPTION W.png");
+		menu_item[3].card = LoadGraph(L"picture/menu/QUIT.png");
+		menu_item[3].ground = LoadGraph(L"picture/menu/QUIT G.png");
+		menu_item[3].back = LoadGraph(L"picture/backstar.png");
+		return;
+	}
+
+	~rec_manu_dataset_c(void) {
+		for (int inum = 0; inum < MENU_NUM; inum++) {
+			DeleteGraph(menu_item[inum].card);
+			DeleteGraph(menu_item[inum].ground);
+			DeleteGraph(menu_item[inum].back);
+		}
+		return;
+	}
+
+	void SetLeft(void) {
+		this->Bcmd = this->Ncmd;
+		this->Ncmd--;
+		if (this->Ncmd < 0) { this->Ncmd += MENU_NUM; }
+		this->LR = -1;
+		this->stime = GetNowCount();
+		return;
+	}
+	
+	void SetRight(void) {
+		this->Bcmd = this->Ncmd;
+		this->Ncmd++;
+		if (MENU_NUM <= this->Ncmd) { this->Ncmd -= MENU_NUM; }
+		this->LR = 1;
+		this->stime = GetNowCount();
+		return;
+	}
+
+	int GetCmd(void) const {
+		return this->Ncmd;
+	}
+
+	void DrawMenu(void) const {
+		this->DrawBack();
+		this->DrawCard();
+		return;
+	}
+};
 
 void RecMenuInitPic(void) {
 	menu_item[0].card = LoadGraph(L"picture/menu/FREE PLAY.png");
@@ -59,15 +147,13 @@ void RecMenuInitPic(void) {
 
 now_scene_t menu(void) {
 	/* 定数 */
-	int command = 0; //選択中のモード
-	int before = 0; //前に選んでたモード
-	int LR = 1;
-	int starttime = -250;
+	int command = 0; //選択したモード
 	int help, goust, select;
 	int key = 1;
 	int	lan[7] = { 0,0,0,2,0,0,0 }; //使うのは[4,言語]だけ
 
 	now_scene_t next = SCENE_MENU; //次のモード
+	rec_manu_dataset_c menu_class;
 	rec_cutin_c cutin;
 
 	FILE *fp;
@@ -79,14 +165,13 @@ now_scene_t menu(void) {
 	unsigned int Cr = GetColor(255, 255, 255);
 	help = LoadGraph(L"picture/help.png");
 	select = LoadSoundMem(L"sound/arrow.wav");
-	RecMenuInitPic();
 	cutin.SetIo(0);
 
 	while (1) {
 		ClearDrawScreen(); /* 描画エリアここから */
 
-		DrawBack(command, before, GetNowCount() - starttime);
-		DrawCard(command, LR, GetNowCount() - starttime);
+		menu_class.DrawMenu();
+
 		RecRescaleDrawGraph(0, 0, help, TRUE);
 		if (lan[4] == 0) {
 			RecRescaleDrawString(5, 460, L"左右キー:選択   Enterキー:決定", Cr);
@@ -103,30 +188,23 @@ now_scene_t menu(void) {
 		if (CheckHitKey(KEY_INPUT_LEFT)) {
 			//左が押された
 			if (key == 0) {
-				before = command;
-				command -= 1;
-				if (command < 0) command += MENU_NUM;
-				LR = -1;
+				menu_class.SetLeft();
 				PlaySoundMem(select, DX_PLAYTYPE_BACK);
-				starttime = GetNowCount();
 			}
 			key = 1;
 		}
 		else if (CheckHitKey(KEY_INPUT_RIGHT)) {
 			//右が押された
 			if (key == 0) {
-				before = command;
-				command += 1;
-				if (MENU_NUM <= command) command -= MENU_NUM;
-				LR = 1;
+				menu_class.SetRight();
 				PlaySoundMem(select, DX_PLAYTYPE_BACK);
-				starttime = GetNowCount();
 			}
 			key = 1;
 		}
 		else if (CheckHitKey(KEY_INPUT_RETURN)) {
 			//エンターが押された
 			if (key == 0) {
+				command = menu_class.GetCmd();
 				if (menu_item[command].num != SCENE_SERECT) {
 					next = menu_item[command].num;
 					break;
@@ -148,6 +226,7 @@ now_scene_t menu(void) {
 		}
 
 		if (cutin.IsEndAnim()) {
+			command = menu_class.GetCmd();
 			next = menu_item[command].num;
 			break;
 		}
@@ -155,33 +234,4 @@ now_scene_t menu(void) {
 		WaitTimer(10);
 	}
 	return next;
-}
-
-void DrawBack(int now, int before, int time) {
-	time = betweens(0, time, 250);
-	RecRescaleDrawGraph(0, 0, menu_item[before].back, TRUE);
-	SetDrawBlendMode(DX_BLENDMODE_ALPHA, lins(0, 0, 250, 255, time));
-	RecRescaleDrawGraph(0, 0, menu_item[now].back, TRUE);
-	SetDrawBlendMode(DX_BLENDMODE_ALPHA, 255);
-	RecRescaleDrawGraph(0, pals(250, 240, 0, 0, time), menu_item[before].ground, TRUE);
-	RecRescaleDrawGraph(0, pals(250, 0, 0, 240, time), menu_item[now].ground, TRUE);
-	return;
-}
-
-void DrawCard(int num, int LR, int time) {
-	time = betweens(0, time, 250);
-	num -= 2;
-	while (num < 0) {
-		num += MENU_NUM;
-	}
-	for (int i = 0; i < 5; i++) {
-		RecRescaleDrawGraph(pals(250, 420 * i - 680, 0, 420 * i + 420 * LR - 680, time),
-			pals(250, 100, -250, 0, -time * LR + 250 * i + 250 * LR - 250),
-			menu_item[num].card, TRUE);
-		num++;
-		if (MENU_NUM <= num) {
-			num -= MENU_NUM;
-		}
-	}
-	return;
 }
