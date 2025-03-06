f780c258aa176f2b9376f0c4497c98cfc74e3937
curbinegames <joint.the.air@gmail.com>
2025-03-06 22:14:08 +0900

    add zoom with bug

--- RecPlay/PlayCamera.h
+++ RecPlay/PlayCamera.h
@@ -14,4 +14,5 @@ extern void DrawGraphRecField(int xpos, int ypos, int pic);
 extern void DrawTurnGraphRecField(int xpos, int ypos, int pic);
 extern void DrawLineRecField(int posx1, int posy1, int posx2, int posy2, unsigned int color, int thick);
 extern void DrawLineCurveRecField(int posx1, int posy1, int posx2, int posy2, int mode, unsigned int color, int thick);
+extern void DrawDeformationPicRecField(int xpos, int ypos, intx100_t size, int rot, int alpha, DxPic_t pic);
 extern void DrawGraphRecBackField(int xpos, int ypos, int pic);
